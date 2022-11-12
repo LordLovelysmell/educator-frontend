687fe16b8cec011dd84645e50ac10145494293bb
Dmitriy Dobrynin <dm.dobrinin@gmail.com>
2022-11-13 00:48:20 +0300

    Add boilerplate for the form validation using yup, add bem-cn for class naming

--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -3,22 +3,44 @@ import { Field, Form, Formik } from "formik";
 import { useAuth } from "@/contexts/AuthContext";
 import { UserData } from "@/services/UserService";
 import { Link } from "react-router-dom";
+import { block } from "bem-cn";
+import Logo from "../../assets/svg/logo.svg";
+import * as Yup from "yup";
+
+const SignupSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(4, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  password: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+});
+
+const authBlock = block("Auth");
+const authClass = authBlock();
+const authHeaderClass = authBlock("header");
 
 const Auth: FC = () => {
   const { signUp, errorMessage } = useAuth();
 
   return (
-    <>
-      <h1>Sign Up</h1>
+    <div className={authClass}>
+      <div className={authHeaderClass}>
+        <Logo />
+        <h1>Sign in to Educator</h1>
+      </div>
       <Formik<UserData>
         initialValues={{
           username: "",
           password: "",
         }}
+        validationSchema={SignupSchema}
         onSubmit={async (values, actions) => {
           try {
-            console.log(values);
-            const token = await signUp(values);
+            await signUp(values);
+            actions.resetForm();
           } catch {}
         }}
       >
@@ -26,11 +48,17 @@ const Auth: FC = () => {
           <Form>
             <Field name="username">
               {({ field, form, meta }: any) => {
+                console.log(props, field, form, meta);
+
                 return (
                   <>
                     <input type="text" placeholder="Username" {...field} />
-                    {errorMessage && (
+                    {/* TODO: Replace below to notification component */}
+                    {/* {errorMessage && (
                       <div className="error">{errorMessage}</div>
+                    )} */}
+                    {props.errors.username && props.touched.username && (
+                      <div>{props.errors.username}</div>
                     )}
                   </>
                 );
@@ -42,7 +70,7 @@ const Auth: FC = () => {
         )}
       </Formik>
       <Link to="/sign-in">Sign In</Link>
-    </>
+    </div>
   );
 };
 
